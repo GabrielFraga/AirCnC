a4b32c2f2a5079060bc97047f8db5728b0058483
GabrielFraga <gabrielgoog@gmail.com>
2020-01-13 22:35:24 -0300

    lint on frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,46 +1,47 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const cors = require('cors')
-const path = require('path')
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const path = require("path");
 
-const socketio = require('socket.io')
-const http = require('http')
+const socketio = require("socket.io");
+const http = require("http");
 
+const routes = require("./routes");
 
-const routes = require('./routes')
+const app = express();
+const server = http.Server(app);
+const io = socketio(server);
 
-const app = express()
-const server = http.Server(app)
-const io = socketio(server)
-
-
-mongoose.connect('mongodb+srv://oministack:oministack@oministack-oexto.mongodb.net/semana_09?retryWrites=true&w=majority', {
+mongoose.connect(
+  "mongodb+srv://oministack:oministack@oministack-oexto.mongodb.net/semana_09?retryWrites=true&w=majority",
+  {
     useNewUrlParser: true,
     useUnifiedTopology: true
-})
+  }
+);
 
-const connectedUsers = {}
+const connectedUsers = {};
 
-io.on('connection', socket => {
-    const { user_id } = socket.handshake.query
+io.on("connection", socket => {
+  const { user_id } = socket.handshake.query;
 
-    connectedUsers[user_id] = socket.id
-})
+  connectedUsers[user_id] = socket.id;
+});
 
 app.use((req, res, next) => {
-    req.io =  io
-    req.connectedUsers = connectedUsers
-    
-    return next()
-})
+  req.io = io;
+  req.connectedUsers = connectedUsers;
+
+  return next();
+});
 
 // req.query = Acessar query params (para filtros)
 // req.params = Acessar route params (para edição, delete)
 // req.body = Acessar corpo da requisição (para criação, edição)
 
-app.use(cors())
-app.use(express.json())
-app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')))
-app.use(routes)
+app.use(cors());
+app.use(express.json());
+app.use("/files", express.static(path.resolve(__dirname, "..", "uploads")));
+app.use(routes);
 
-server.listen(3333)
+server.listen(3333);
